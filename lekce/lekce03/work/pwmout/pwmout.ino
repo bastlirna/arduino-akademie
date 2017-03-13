f932df269f6c6821fbb445d6e06f125fb6bb1a40
ah01 <horcicaa@gmail.com>
2017-03-13 22:03:07 +0100

    Kód z lekce 3

---
+++ lekce/lekce03/work/pwmout/pwmout.ino
@@ -0,0 +1,15 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(10, OUTPUT);
+}
+
+void loop() {
+
+  for(int i = 0; i <= 0xFF; i++)
+  {
+    Serial.println(i, DEC);
+    analogWrite(10, i);
+    delay(100);
+  }
+
+}
---
+++ lekce/lekce03/work/rgb/rgb.ino
@@ -0,0 +1,25 @@
+#define RGB_R 9
+#define RGB_G 5
+#define RGB_B 6
+
+void setup() {
+  pinMode(RGB_R, OUTPUT);
+  analogWrite(RGB_R, 255);
+}
+
+void color(byte r, byte g, byte b) {
+  analogWrite(RGB_R, 255 - r);
+  analogWrite(RGB_G, 255 - g);
+  analogWrite(RGB_B, 255 - b);
+}
+
+void loop() {
+  for (int r = 0; r <= 255; r += 64) {
+    for (int g = 0; g <= 255; g += 64) {
+      for (int b = 0; b <= 255; b += 64) {
+        color(r, g, b);
+        delay(200);
+      }
+    }
+  }
+}
