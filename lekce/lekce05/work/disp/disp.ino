1db47a52cf2479750452ddb92f46a1a5eaa7f19b
ah01 <horcicaa@gmail.com>
2017-03-27 23:15:38 +0200

    Kód z lekce

---
+++ lekce/lekce05/work/disp/disp.ino
@@ -0,0 +1,22 @@
+#include <Wire.h>
+#define ADDR 0x27
+
+void setup() {
+  Wire.begin();
+}
+
+void loop() {
+  digit(0, 0b00101101);
+  digit(1, 0b00101101);
+  digit(2, 0b00101101);
+  digit(3, 0b00101101);
+  delay(1000);
+}
+
+void digit(byte n, byte s) {
+  Wire.beginTransmission(ADDR);
+  Wire.write(n); // <- pozice
+  Wire.write(s); // <- segmenty
+  Wire.endTransmission();
+}
+
